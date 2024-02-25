8a57b5bd43012ed824cb4c816c4cdc1ead2613bd
Aulkanic <trazonamichael.pdm@gmail.com>
2024-02-25 11:46:01 +0800

    login

--- Go2rismo/src/layout/private.tsx
+++ Go2rismo/src/layout/private.tsx
@@ -3,16 +3,15 @@ import React, { useCallback, useState } from 'react'
 import { RouterUrl } from '../routes';
 import clsx from 'clsx';
 import Logo from '../../src/assets/app logo.png'
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Navigate, Outlet, useNavigate } from 'react-router-dom';
 import useStore from '../zustand/store/store';
-import { selector } from '../zustand/store/store.provide';
+import { logoutAdmin, logoutBusiness, logoutTraveller, removeUserType, selector } from '../zustand/store/store.provide';
 
 export default function Private(){
-  const typeUser:string = 'admi';
+  const navigate = useNavigate()
   const user = useStore(selector('user'))
   const [selected,setSelected] = useState(0)
-  console.log(user)
-  const links = user.userType === 'traveller' ?
+  const links = user?.userType === 'traveller' ?
   [
     {id:0,name:'Home',url:RouterUrl.TRAVELLERSIDE},
     {id:1,name:'Booking',url:RouterUrl.TRAVELLERBOOKING},
@@ -21,7 +20,7 @@ export default function Private(){
     {id:4,name:'Adventure',url:RouterUrl.TRAVELLERADVENTURE},
     {id:5,name:'Maps',url:RouterUrl.TRAVELLERMAPS},
     {id:6,name:'Profile',url:RouterUrl.TRAVELLERPROFILE},
-  ] : typeUser === 'admin' ?
+  ] : user?.userType === 'admin' ?
   [
     {id:0,name:'Home',url:RouterUrl.ADMINSIDE},
     {id:1,name:'Booking',url:RouterUrl.ADMINBOOKING},
@@ -39,7 +38,14 @@ export default function Private(){
    const onSeletedMenu = useCallback((item:any) =>{
       setSelected(item.id)
    },[selected])
-  return (
+   const handleLogout = () =>{
+    removeUserType()
+    user.userType === 'traveller' ? logoutTraveller() : user.userType === 'business' ? logoutBusiness : logoutAdmin()
+    navigate('/Login')
+   }
+  return (!user?.isLogin) ? (
+    <Navigate replace to={RouterUrl.LOGIN} />
+  ) : (
     <div>
       <header className='bg-[#41E8D1] px-4 py-2 flex justify-between items-center'>
         <div className='flex gap-2 items-center'>
@@ -59,6 +65,8 @@ export default function Private(){
                 </li>
               )
             })}
+            <li onClick={handleLogout}
+            className='text-[#00256E] hover:text-white cursor-pointer font-bold'>Logout</li>
           </ul>
         </div>
       </header>
