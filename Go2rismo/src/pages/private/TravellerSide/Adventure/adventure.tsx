1854e210a1129675ba3d67f8e9eb76bb90392137
Aulkanic <trazonamichael.pdm@gmail.com>
2024-06-05 21:47:18 +0800

    dsda

--- Go2rismo/src/pages/private/TravellerSide/Adventure/adventure.tsx
+++ Go2rismo/src/pages/private/TravellerSide/Adventure/adventure.tsx
@@ -30,22 +30,21 @@ export const TravelAdventure = () => {
    console.log(travels)
   return (
     <div id='adv' className='px-4 md:px-16 py-8'>
-      <p className='w-full text-center text-[25px] font-[700] text-[#060E61]'>My Adventure</p>
+      <p className='w-full text-center text-[25px] font-[700] text-[#060E61] mb-8'>My Adventure</p>
       <Swiper
         navigation={true}
-        spaceBetween={30}
         breakpoints={{
           640: {
             slidesPerView: 2,
             spaceBetween: 20,
           },
           768: {
-            slidesPerView: 2,
-            spaceBetween: 40,
+            slidesPerView: 4,
+            spaceBetween: 20,
           },
           1024: {
-            slidesPerView: 2,
-            spaceBetween: 50,
+            slidesPerView: 4,
+            spaceBetween: 20,
           },
         }}
         slidesPerView={1}
@@ -56,16 +55,14 @@ export const TravelAdventure = () => {
       >
         {travels?.map((h:any,idx:number) =>{
           return(
-          <SwiperSlide key={idx} className='px-16 py-8 h-full'>
+          <SwiperSlide key={idx} className='h-full'>
               <div onClick={() => navigate(`/UserDashBoard/HomePage/${h.type}/${h.name}`)} className='rounded-xl cursor-pointer h-full'>
-                <p>{h.name}</p>
+                
                 <div className='relative flex flex-col items-center justify-center w-full'>
-                  <img className='w-full h-full' src={h.photos[0] || ''} alt="" />
-                  {(h.location && h.address) && <p className='w-full text-left text-[14px] m-0'>Location: {h.address} {h.location}</p>}
+                  <img className='w-[220px] h-[220px]' src={h.photos[0] || ''} alt="" />
+                  <p className='absolute bottom-2 w-[90%] bg-white'>{h.name}</p>
                 </div>
-                <div className='w-full h-full text-[15px] font-[400] line-clamp-5 flex justify-start text-left items-top rounded-b-xl'>
-                  {h.description}
-                </div>
+              
               </div>
               </SwiperSlide>
             )})}
